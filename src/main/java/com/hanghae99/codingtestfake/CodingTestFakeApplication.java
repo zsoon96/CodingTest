6165ce267822734d664b18a2c439fc815f36b199
zsoon96 <pres0616@gmail.com>
2022-06-19 00:32:31 +0900

    first commit

---
+++ src/main/java/com/hanghae99/codingtestfake/CodingTestFakeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae99.codingtestfake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestFakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestFakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae99/codingtestfake/CodingTestFakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.codingtestfake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestFakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

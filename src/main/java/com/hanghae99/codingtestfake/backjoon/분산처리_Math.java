3ee83035d3583d009639bb85ffc0eed81b9bdf5a
zsoon96 <pres0616@gmail.com>
2022-06-29 00:14:08 +0900

    분산처리_Math 계산 값 오류

--- src/main/java/com/hanghae99/codingtestfake/backjoon/분산처리_Math.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/분산처리_Math.java
@@ -5,25 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 분산처리 {
+public class 분산처리_Math {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for ( int i = 0; i < n; i ++ ){
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int answer = 1;
+            // a의 b승 값 구하고, 1의 자리 수 구하기
+            int num = ((int) Math.pow(a,b)) % 10;
+            // 1의 자리 수가 0이면 10으로 리턴
+            num = num == 0 ? 10 : num;
 
-            for ( int j = 1; j <= b; j++ ) {
-                answer = answer * a % 10;
-            }
-            answer = answer == 0 ? 10 : answer;
-
-            System.out.println(answer);
+            System.out.println(num);
         }
-
     }
 }

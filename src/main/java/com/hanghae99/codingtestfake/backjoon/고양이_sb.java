811826ce404453ede7fb361556c03f87dd00d6e3
zsoon96 <pres0616@gmail.com>
2022-06-28 15:42:59 +0900

    고양이_stringbuilder

---
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/고양이_sb.java
@@ -0,0 +1,14 @@
+package com.hanghae99.codingtestfake.backjoon;
+
+public class 고양이_sb {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+
+        System.out.println(sb);
+    }
+}

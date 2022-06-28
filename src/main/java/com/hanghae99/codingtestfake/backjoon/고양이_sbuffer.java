cdad40697178e5f7b1bf24a83e5c78643145f482
zsoon96 <pres0616@gmail.com>
2022-06-28 15:44:40 +0900

    고양이_stringbuffer

---
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/고양이_sbuffer.java
@@ -0,0 +1,14 @@
+package com.hanghae99.codingtestfake.backjoon;
+
+public class 고양이_sbuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+
+        System.out.println(sb);
+    }
+}

2a2c5d2be5f9753d57235ea819fd05555ec98ac2
zsoon96 <pres0616@gmail.com>
2022-06-28 15:15:49 +0900

    A+B_buffer

---
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/AplusB.java
@@ -0,0 +1,21 @@
+package com.hanghae99.codingtestfake.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AplusB {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader ( new InputStreamReader(System.in) );
+        StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        answer = a + b;
+        System.out.println(answer);
+    }
+}

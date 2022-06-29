9fe054a2e1b8b0777e28df8dd5c451fc24441324
zsoon96 <pres0616@gmail.com>
2022-06-29 22:51:24 +0900

    괄호

--- src/main/java/com/hanghae99/codingtestfake/backjoon/stack/괄호.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/stack/괄호.java
@@ -1,27 +1,39 @@
-//package com.hanghae99.codingtestfake.backjoon.stack;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.Stack;
-//
-//public class 괄호 {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int n = Integer.parseInt(br.readLine());
-//
-//        Stack<Integer> input = new Stack<>();
-//
-////        for ( int i = 0; i < n; i++ ){
-////            input.add(Integer.parseInt(br.readLine()));
-////        }
-//
-//        for ( int i = 0; i < n; i++ ) {
-//            char t = br.readLine();
-//
-//            if ( t == )
-//
-//        }
-//    }
-//}
+package com.hanghae99.codingtestfake.backjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 괄호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String answer = "NO";
+
+        Stack<Character> input = new Stack<>();
+
+        String t = br.readLine();
+        for ( int i = 0; i < n; i++ ) {
+            char tt = t.charAt(i);
+
+            if ( tt == '(' ) {
+                input.push(tt);
+            }
+            else if (input.isEmpty()) {
+                System.out.println(answer);
+                break;
+            }
+            else input.pop();
+
+            if ( input.isEmpty() ) {
+                answer = "YES";
+            } else {
+                answer = "NO";
+            }
+            System.out.println(answer);
+        }
+
+    }
+}

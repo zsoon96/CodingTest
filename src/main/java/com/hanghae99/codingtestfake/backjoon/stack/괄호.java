a0e50eb674c63d76f206f7c5f457bc9d73689c91
zsoon96 <pres0616@gmail.com>
2022-06-29 23:35:40 +0900

    괄호2_런타임에러

--- src/main/java/com/hanghae99/codingtestfake/backjoon/stack/괄호.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/stack/괄호.java
@@ -10,30 +10,43 @@ public class 괄호 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        String answer = "NO";
+
+        // 추가 출력을 한번에 묶어 출력하도록 StringBuffer 사용
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(solve(br.readLine())).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    // 검사해주는 함수 작성
+    public static String solve(String s) {
 
         Stack<Character> input = new Stack<>();
 
-        String t = br.readLine();
-        for ( int i = 0; i < n; i++ ) {
-            char tt = t.charAt(i);
+        for ( int i = 0; i < s.length(); i++ ) {
+            char ss = s.charAt(i);
 
-            if ( tt == '(' ) {
-                input.push(tt);
+            // 여는 괄호일 때 추가
+            if ( ss == '(' ) {
+                input.push(ss);
             }
-            else if (input.isEmpty()) {
-                System.out.println(answer);
-                break;
+
+            // 닫는 괄호일 때 삭제
+            else if (input.empty()) {
+                return "NO";
             }
             else input.pop();
-
-            if ( input.isEmpty() ) {
-                answer = "YES";
-            } else {
-                answer = "NO";
-            }
-            System.out.println(answer);
         }
 
+        // 검사 후 스택이 비어 있으면 YES
+        if ( input.empty() ) {
+            return "YES";
+        }
+
+        else {
+            return "NO";
+        }
     }
 }
---
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/stack/괄호2.java
@@ -0,0 +1,49 @@
+package com.hanghae99.codingtestfake.backjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 괄호2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Character> input = new Stack<>();
+
+        // 한 줄씩 확인
+        for ( int i = 0; i < n; i++ ){
+            String line = br.readLine();
+            int length = line.length();
+
+            for ( int j = 0; j < length; j++ ) {
+                char text = line.charAt(i);
+
+                if ( text == '(') {
+                    input.push(text);
+                }
+
+                else {
+                    if ( input.size() == 0 ) {
+                        input.push(text); // 비어있을 때 닫는 괄호를 넣고 바로 break - false
+                        break;
+                    }
+                    else {
+                        input.pop(); // 있으면 삭제
+                    }
+                }
+            }
+
+            if ( input.isEmpty() ) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+
+            input.clear();
+        }
+    }
+}

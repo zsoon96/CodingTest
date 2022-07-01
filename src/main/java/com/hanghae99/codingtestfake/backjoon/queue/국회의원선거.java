a8bb56024539f231cd901bfc98d86bcfad707ba0
zsoon96 <pres0616@gmail.com>
2022-07-01 11:32:10 +0900

    국회의원선거_해결

--- src/main/java/com/hanghae99/codingtestfake/backjoon/queue/국회의원선거.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/queue/국회의원선거.java
@@ -1,5 +1,3 @@
-package com.hanghae99.codingtestfake.backjoon.queue;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,26 +12,29 @@ public class 국회의원선거 {
         int cnt = 0;
 
         // 내림차순 정렬
-        int [] vote = new int[n-1];
-        for (int i=0; i<n-1; i++) {
+        int[] vote = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
             vote[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i=0; i<n-1; i++) {
+        // 배열이 비였을 때, stop하고 출력
+        if (vote.length == 0) {
+            System.out.println(cnt);
+            return;
+        }
 
-            if ( vote.length != 0 ) {
-                Arrays.sort(vote);
-//                int max = vote[vote.length-1];
-                int max = vote.length-1;
+        // 반복문 범위 수정 ( for문으로 하면 뺏어 올수 있는 횟수가 제한됨 )
+        while (true) {
+            Arrays.sort(vote);
+            int max = vote.length - 1;
 
-                if (me <= vote[max]) {
-                    vote[max]--;
-                    me ++;
-                    cnt++;
-                }
-            }
-            else {
-                cnt = 0;
+            if (me <= vote[max]) {
+                vote[max]--;
+                me++;
+                cnt++;
+            } else {
+                // break 있어야 출력 없으면 무한루프 ^^
+                break;
             }
         }
         System.out.println(cnt);

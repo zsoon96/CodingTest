d288c2f8d1cc0c085e8508df12adb17413d6eb6f
zsoon96 <pres0616@gmail.com>
2022-07-19 20:34:13 +0900

    별찍기7_StringBuilder활용

--- src/main/java/com/hanghae99/codingtestfake/backjoon/basic/별찍기7_sb.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/basic/별찍기7_sb.java
@@ -4,35 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 별찍기7 {
+public class 별찍기7_sb {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         for ( int i = 1; i <= n; i++ ) {
             for ( int j = i; j <= n-1; j++ ) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for ( int k = 1; k <= i; k++ ) {
-                System.out.print("*");
+                sb.append("*");
             }
             for ( int f = 2; f <= i; f++ ){
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
 
         for ( int i = n-1; i > 0; i-- ){
             for (int j = i; j < n; j++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for ( int k = i; k > 0; k-- ){
-                System.out.print("*");
+                sb.append("*");
             }
             for ( int f = i-1; f > 0; f--) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 }

5c7009a1fe96c7d0fa2369864f6fdaf4624c3579
zsoon96 <pres0616@gmail.com>
2022-07-21 16:09:57 +0900

    18310ㅣ안테나_정답2

--- src/main/java/com/hanghae99/codingtestfake/backjoon/array/안테나.java
+++ src/main/java/com/hanghae99/codingtestfake/backjoon/array/안테나.java
@@ -10,7 +10,6 @@ public class 안테나 {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int answer = 0;
 
         int [] num = new int [n];
 
@@ -23,16 +22,6 @@ public class 안테나 {
         // 배열을 오름차순 해놓고, 가운데 인덱스의 위치가 거리의 최솟값이 나오게 됨
         // 그렇기 때문에 배열을 오름차순 해놓고, 가운데 인덱스 반환해서 해결
         Arrays.sort(num);
-
-        // 주의, 배열의 길이가 짝수일 경우, 최소값이 2개 나오기 때문에 둘 중 앞요소로 출력해줘야함
-        if ( (num.length % 2) == 0 ) {
-            answer = num[num.length/2 -1];
-        }
-        // 배열의 길이가 홀수일 경우, 가운데 인덱스 반환
-        else {
-            answer = num[num.length/2];
-        }
-
-        System.out.println(answer);
+        System.out.println(num[(n-1)/2]);
     }
 }

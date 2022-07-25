bd08548cc81edb16e5cf965f927a5aefee5673ee
zsoon96 <pres0616@gmail.com>
2022-07-25 23:59:14 +0900

    그리디ㅣ2839 설탕배달_오답

---
+++ src/main/java/com/hanghae99/codingtestfake/greedy/설탕배달_2839.java
@@ -0,0 +1,31 @@
+package com.hanghae99.codingtestfake.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 설탕배달_2839 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        while ( n > 0 ) {
+            // 5로 바로 나누어 떨어지면 게임 끝
+            if ( n % 5 == 0 ) {
+                answer += n / 5;
+                System.out.println(answer);
+                break;
+            }
+            // 아니면 3을 빼주고 +1
+            else {
+                n -= 3;
+                answer ++;
+            }
+            // 만약 n이 음수가 나오면 -1 출력
+            if ( n < 0 ) {
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}

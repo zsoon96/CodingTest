ee0dab01369876f227c90a7de8aad784f728366f
zsoon96 <pres0616@gmail.com>
2022-07-27 01:52:40 +0900

    그리디ㅣ2217 로프_정답 코드

--- src/main/java/com/hanghae99/codingtestfake/greedy/로프_2217.java
+++ src/main/java/com/hanghae99/codingtestfake/greedy/로프_2217.java
@@ -3,6 +3,7 @@ package com.hanghae99.codingtestfake.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class 로프_2217 {
     public static void main(String[] args) throws IOException {
@@ -14,17 +15,18 @@ public class 로프_2217 {
             k[i] = Integer.parseInt(br.readLine());
         }
 
-        int max = 0;
-        int a = k[0]; // 최소 무게
+        // 낮은 무게 순으로 정렬
+        Arrays.sort(k);
 
-        for ( int i = 0; i < n; i++ ) {
-            // 현재 무게가 최소 무게보다 작으면 갱신
-            if ( k[i] < a ) {
-                a = k[i];
-            }
+        int max = 0;
+        // 무거운 순으로 가장 최대의 중량 구하기
+        // 주의) 모든 로프를 사용하지 않아도 됨 !
+        for ( int i = n-1; i >= 0; i-- ) {
+            // 현재 중량(k[i])과 병렬 중량이 가능한 경우의 수(n-i) 곱해주고,
+            // 기존의 max값과 현재 도출한 값 중에서 최대 값으로 max값 갱신
+            k[i] = k[i] * (n-i); // 1, 2, 3 ...
+            max = Math.max(max, k[i]);
         }
-        // 최소 무게와 로프의 개수를 구한 후 출력
-        max = a * n;
         System.out.println(max);
     }
 }

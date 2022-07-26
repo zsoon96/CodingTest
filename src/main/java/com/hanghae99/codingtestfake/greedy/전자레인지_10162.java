4ab8d37098b43b565a67215deef3df4e6188f9cb
zsoon96 <pres0616@gmail.com>
2022-07-26 21:37:42 +0900

    그리디ㅣ10162 전자레인지_100점

--- src/main/java/com/hanghae99/codingtestfake/greedy/전자레인지_10162.java
+++ src/main/java/com/hanghae99/codingtestfake/greedy/전자레인지_10162.java
@@ -12,25 +12,28 @@ public class 전자레인지_10162 {
         int b = 0;
         int c = 0;
 
+        // n이 제일 큰 수(300)로 나눠지기 전까지 작은 수(60, 10)로 빼주기
         while (true) {
-            if ( n % 10 != 0 ) {
-                System.out.println(-1);
-                break;
-            }
-
-            else if ( n % 300 == 0 ) {
+            // 60 또는 10으로 빼주고 300으로 나눠지면 한방 승부
+            if ( n % 300 == 0 ) {
                 a = n / 300;
                 System.out.println(a + " " + b + " " + c);
                 break;
             }
+            // 60으로 나눠지면 -60 cnt +1
             else if ( n % 60 == 0 ) {
-                b = n / 60;
-                System.out.println(a + " " + b + " " + c);
-                break;
+                n -= 60;
+                b++;
             }
-            else {
+            // 10으로 나눠지면 -10 cnt +1
+            else if ( n % 10 == 0 ) {
                 n -= 10;
                 c++;
+            }
+            // 위 조건에 해당이 안되면 -1 출력
+            else {
+                System.out.println(-1);
+                break;
             }
         }
     }

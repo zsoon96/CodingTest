260b48898789fcfadc19285b6bb4c276b9b78bde
zsoon96 <pres0616@gmail.com>
2022-07-28 15:01:26 +0900

    구현ㅣ상하좌우_정답 코드

--- src/main/java/com/hanghae99/codingtestfake/implement/상하좌우2.java
+++ src/main/java/com/hanghae99/codingtestfake/implement/상하좌우2.java
@@ -9,47 +9,36 @@ public class 상하좌우2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        String [] input = br.readLine().split(" ");
         int x = 1;
         int y = 1;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] dx = {0,0,-1,1}; // 상하
+        int [] dy = {-1,1,0,0}; // 좌우
+        String [] position = {"L","R","U","D"};
 
-        // 입력값이 L이면 x-1
-        if ( "L".equals(st.nextToken())) {
-            if ( y > 1 ) {
-                y -= 1;
+        for ( int i = 0; i < input.length; i++ ) {
+            // 위치 초기값 설정
+            int nx = 1;
+            int ny = 1;
+
+            // 입력 값에 맞는 위치만큼 이동
+            for ( int j = 0; j < 4; j++ ) {
+                if ( input[i].equals(position[j]) ) {
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
             }
-            else {
-                y -= 0;
+
+            // 공간 범위 벗어 날 경우
+            // 해당 조건에 부합하면 이후 반복문 내부의 수행문 무시
+            if ( nx < 1 || ny < 1 || nx > n || ny > n ) {
+                continue;
             }
+
+            x = nx;
+            y = ny;
         }
-        // 입력값이 R이면 x+1
-        if ("R".equals(st.nextToken())) {
-            if ( y < n ) {
-                y += 1;
-            } else {
-                y += 0;
-            }
-        }
-        // 입력값이 U이면 y-1
-        if ( "U".equals(st.nextToken())) {
-            if ( x > 1 ) {
-                x -= 1;
-            }
-            else {
-                x -= 0;
-            }
-        }
-        // 입력값이 D이면 y+1
-        if ( "D".equals(st.nextToken())) {
-            if ( x < n ) {
-                x += 1;
-            }
-            else {
-                x += 0;
-            }
-        }
-        // 값이 1보다 작거나 n보다 크면 제자리
-        System.out.println( x + " " + y);
+        System.out.println( x + " " + y );
     }
 }

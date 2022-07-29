425558719c79355268027cb11b9fd607683e2b21
zsoon96 <pres0616@gmail.com>
2022-07-29 14:08:10 +0900

    구현ㅣ3046 R2_정답

---
+++ src/main/java/com/hanghae99/codingtestfake/implement/R2_3046.java
@@ -0,0 +1,18 @@
+package com.hanghae99.codingtestfake.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class R2_3046 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        System.out.println((s * 2) - r1);
+    }
+}

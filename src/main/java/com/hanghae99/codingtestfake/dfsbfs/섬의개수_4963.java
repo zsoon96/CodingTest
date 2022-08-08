fd693cce438857c25854a8e98991aeb6da48bd5c
zsoon96 <pres0616@gmail.com>
2022-08-09 01:51:17 +0900

    DFSㅣ4963 섬의 개수_정답 코드

--- src/main/java/com/hanghae99/codingtestfake/dfsbfs/섬의개수_4963.java
+++ src/main/java/com/hanghae99/codingtestfake/dfsbfs/섬의개수_4963.java
@@ -9,52 +9,61 @@ public class 섬의개수_4963 {
 
     static int w,h;
     static int[][] map;
+    static boolean[][] visit;
     static int[] dx = {-1,1,0,0,-1,1,-1,1};
     static int[] dy = {0,0,-1,1,-1,-1,1,1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        w = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
+        // 테스트 별 반복
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
 
-        map = new int[w][h];
-        int cnt = 0;
+            map = new int[h][w];
+            visit = new boolean[h][w];
+            int cnt = 0;
 
-
-        // 지도 그리기
-        for ( int i = 0; i < w; i++ ) {
-            st = new StringTokenizer(br.readLine());
-            for ( int j = 0; j < h; j++ ) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            // 지도 너비와 높이가 0일 경우애는 종료
+            if ( w == 0 && h == 0 ) {
+                break;
             }
-        }
 
-        for ( int i = 0; i < w; i++ ) {
-            for ( int j = 0; j < h; j++ ) {
-                if ( map[i][j] == 1 ) {
-                    dfs(i, j);
-                    cnt++;
+            // 지도 그리기
+            for ( int i = 0; i < h; i++ ) {
+                st = new StringTokenizer(br.readLine());
+                for ( int j = 0; j < w; j++ ) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+
+            for ( int i = 0; i < h; i++ ) {
+                for ( int j = 0; j < w; j++ ) {
+                    if ( map[i][j] == 1 && !visit[i][j] ) { // 방문 여부를 체크해야지 첫 수가 0일 때도 실행됨!
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
         }
-        System.out.println(cnt);
+
     }
 
     public static void dfs(int a, int b) {
-        map[a][b] = 1;
+        visit[a][b] = true;
 
         for ( int i = 0; i < 8; i++ ) {
             int nx = a + dx[i];
             int ny = b + dy[i];
 
-            if ( nx < 0 || nx >= w || ny < 0 || ny >= h ) {
+            if ( nx < 0 || nx >= h || ny < 0 || ny >= w ) {
                 continue;
             }
 
-            if ( map[nx][ny] == 1 ) {
+            if ( map[nx][ny] == 1 && !visit[nx][ny] ) {
                 dfs(nx, ny);
             }
         }
